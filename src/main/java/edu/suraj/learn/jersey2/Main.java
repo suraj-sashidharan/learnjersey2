d0140884e93649e216f4bda1f9e34943e325ef8e
suraj-sashidharan <suraj_sashidharan@yahoo.com>
2019-10-03 09:51:10 -0400

    Initial commit

---
+++ src/main/java/edu/suraj/learn/jersey2/Main.java
@@ -0,0 +1,46 @@
+package edu.suraj.learn.jersey2;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ * SURAJ SAYS :
+ * Guide/Documentation for Jersey2 : https://eclipse-ee4j.github.io/jersey.github.io/documentation/latest/user-guide.html *
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in edu.suraj.learn.jersey2 package
+        final ResourceConfig rc = new ResourceConfig().packages("edu.suraj.learn.jersey2");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
+    }
+}
+
